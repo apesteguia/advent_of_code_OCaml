a4a693d64938646e4859ef40beaa506c71950a13
apesteguia <mikeltxomikeltxo@gmail.com>
2024-03-01 20:47:01 +0100

    day1

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+open Advent_of_code_OCaml
+
+let () =
+  let a =
+    Files.read_lines "~/Escritorio/advent_of_code_OCaml/bin/inputs/day1.txt"
+  in
+  List.iter (fun i -> print_endline i) a
---
+++ lib/files.ml
@@ -0,0 +1,13 @@
+let read_lines path : string list =
+  let ic = open_in path in
+  let try_read () = try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc =
+    match try_read () with
+    | Some s -> loop (s :: acc)
+    | None ->
+        close_in ic;
+        List.rev acc
+  in
+  loop []
+
+let print_lines list = List.iter (fun line -> print_endline line) list
