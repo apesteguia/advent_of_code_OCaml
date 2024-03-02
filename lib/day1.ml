1882b5e387ed60927823a3ed602c4d4759aa679d
apesteguia <mikeltxomikeltxo@gmail.com>
2024-03-02 15:24:56 +0100

    day1

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,7 +1,21 @@
 open Files
 
-let rec find_first_num (xs : char list) : int =
-  match xs with
-  | h :: _ when is_digit h -> int_of_char h
-  | _ :: t -> find_first_num t
-  | [] -> raise Not_found
+let find_first_num str : int =
+  let len = String.length str in
+  let found_index = ref (-1) in
+  try
+    for i = 0 to len - 1 do
+      if is_digit str.[i] then (
+        found_index := i;
+        raise Exit)
+    done;
+    -1
+  with Exit -> int_of_char str.[!found_index]
+
+let solve xs = 
+  let rec aux xs acc = 
+    match xs with 
+    | [] -> 0
+    | h :: t -> let num = find_first_num h in 
+      aux t num
+  
